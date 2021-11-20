0abc6d549e7740d5e9e9456f8a5a0df70925d7ef
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-20 10:57:52 +0700

    Using a Bitmap

--- app/src/main/java/com/rujirakongsomran/kt_scanwithmlkit/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_scanwithmlkit/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.rujirakongsomran.kt_scanwithmlkit
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.common.InputImage
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,11 @@ class MainActivity : AppCompatActivity() {
 
         val options = BarcodeScannerOptions.Builder()
             .setBarcodeFormats(
-                Barcode.FORMAT_QR_CODE)
+                Barcode.FORMAT_QR_CODE
+            )
             .build()
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.qrcode)
+        val image = InputImage.fromBitmap(bitmap, 0)
     }
 }
