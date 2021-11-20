ee2c8dc5d187703e581c0dacb9a575a4a7fa57e6
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-20 09:42:21 +0700

    Configure the barcode scanner

--- app/src/main/java/com/rujirakongsomran/kt_scanwithmlkit/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_scanwithmlkit/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rujirakongsomran.kt_scanwithmlkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE)
+            .build()
     }
 }
